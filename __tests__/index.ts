00cead0712f8c3f84396baf73470a957ca9daeed
JayaKrishna <namburu1995@gmail.com>
2020-01-30 11:32:43 +0530

    Adding test cases

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,4 +1,5 @@
 import { markdownUIDLGenerator } from '../src/index'
+import { UIDLElementNode, UIDLElement } from '@teleporthq/teleport-types'
 
 describe('Parsing markdown and generating UIDL Nodes', () => {
   const generator = markdownUIDLGenerator()
@@ -9,10 +10,56 @@ describe('Parsing markdown and generating UIDL Nodes', () => {
     expect(uidl.content.children.length).toBe(0)
   })
 
-  it('Parses markdown with heading and generates UIDLElementNode', () => {
+  it('Parses and generates UIDLElementNode with Heading', () => {
     const markdown = `# Heading`
     const uidl = generator.parse(markdown)
 
     expect(uidl.content.children.length).toBeGreaterThan(0)
+  })
+
+  it('Parses and generates UIDLElementNode with paragraph', () => {
+    const markdown = `
+# Heading
+This is pragraph`
+    const uidl = generator.parse(markdown)
+    const heading = uidl.content.children[0] as UIDLElementNode
+    const paragraph = uidl.content.children[1] as UIDLElementNode
+
+    expect(uidl.content.children.length).toBe(2)
+    expect(heading.content.elementType).toBe('h1')
+    expect(paragraph.content.elementType).toBe('textblock')
+  })
+
+  it('Parses and generates UIDLEmenetNodes with anchor tag', () => {
+    const markdown = `
+## Heading
+[link](https://www.google.com)`
+    const uidl = generator.parse(markdown)
+    const heading = uidl.content.children[0] as UIDLElementNode
+    const paragraph = uidl.content.children[1].content as UIDLElement
+    const link = paragraph.children[0] as UIDLElementNode
+
+    expect(uidl.content.children.length).toBe(2)
+    expect(heading.content.elementType).toBe('h2')
+    expect(link.content.elementType).toBe('link')
+    expect(link.content.attrs.url.content).toBe('https://www.google.com')
+  })
+
+  it('Parses and generates UIDLEmenetNodes with anchor tag', () => {
+    const markdown = `
+### Heading
+![Alt Text](https://avatars1.githubusercontent.com/u/33519579?s=200&v=4 "Teleport")`
+    const uidl = generator.parse(markdown)
+    const heading = uidl.content.children[0] as UIDLElementNode
+    const paragraph = uidl.content.children[1].content as UIDLElement
+    const image = paragraph.children[0] as UIDLElementNode
+
+    expect(uidl.content.children.length).toBe(2)
+    expect(heading.content.elementType).toBe('h3')
+    expect(image.content.elementType).toBe('image')
+    expect(image.content.attrs.url).toBe(
+      'https://avatars1.githubusercontent.com/u/33519579?s=200&v=4'
+    )
+    expect(image.content.attrs.alt).toBe('Alt Text')
   })
 })
