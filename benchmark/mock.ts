6680f211a4e67135d4f0cff6c05795026953c169
JayaKrishnaNamburu <namburu1995@gmail.com>
2020-02-25 21:56:12 +0530

    Added benchmarking results

---
+++ benchmark/mock.ts
@@ -0,0 +1,105 @@
+export const markdown = `
+# Tu omnia
+
+## Datam nec Iole ferarum dare
+
+Lorem markdownum pertimui in nisi vitibus coniugis insomni, est quam enim
+metitur sidera. Aera onerosus sonuere ibitis adhuc rates tuo Rhodanumque cornua
+procul functa pater. Cum frigora: subit in voluere **caede insequitur
+discidium** soli **excessisse est falsae** eodem.
+
+    var p_gateway_parameter = vle_webcam_contextual(raw, servletWorkstationWeb)
+            + network * -1;
+    var scalable_memory_utf = teraflopsCifs(remote, network_integer) - dragZif -
+            multitasking_eps;
+    var hoverWorkstationPortal = networkStorageTarget;
+
+Supersunt *ossibus* scopulum simili ignes est intexere Pylios duorum remanet
+quaedam caelestibus sibi, decerpsit ab Pandion iraque. Aut modo; via [vertitur
+Neptunia](http://tirynthia.net/), est? Spiro quascumque ardua os cives super per
+rotae perque sed inexperrectus virgine Perseidos hunc vesana atras suus, haec.
+Globos haud dextra aditus capiunt aquarum novissimus neve: neve vias incautus
+penetralia tela doloris haesit.
+
+## Per arte disiectisque sunt hoc animique bracchia
+
+Rigebant ligavit, sors suis si secuta, purpureum quoque, est prodesse cedere
+interiit retinentia ferus infelix. Concipiunt Caune superum, monilia premis
+dedissent? De ora factaque quercus, Nixosque at fugam ora more cava. Ambo quo
+**successisse docti**?
+
+    sms_visual.ebookSimmMenu.insertion(keywordsProtocolBackup, 1);
+    var browser = firewire;
+    if (backbone_skin_ivr <= hardRowDigital - -2) {
+        ospfProcessor = oasis - 536725;
+        dv_banner_of -= model(88);
+    }
+    if (version_dns.ssid_soft_ram.ramHard(batchLinkMulti + cdRecursionBackside,
+            pushAspIpv)) {
+        emulationEcc -= flash_lock(dvi * 2);
+        saas_hypermedia = ip(linkedinSuperscalarExpress);
+    } else {
+        heatStandaloneSpyware.t -= subdirectory_mac.dvd(soft_start + namespace);
+        yahoo += server_sector;
+    }
+
+Coma versis, mihi potitus, timentem factas noverat sceleri poterat; cum de enim
+aspera venit? In macies inpatiens fuere speratque, est fratres cremabis; abiit
+iter natas Erinys debere et fugitque pedibus, est. Formosa morbo spiramenta
+luctor.
+
+Meque agi qui est indignantem orbem piget scopulum: seu agros, morte peto.
+Admota canitie, me **divino invenit pariter** aequore caelo.
+
+# Certe sunt non tibia longa tulit tollit
+
+## Sed genitoris putes
+
+Lorem markdownum poterat? In accensa posse fiducia ipso. Terrae cesserunt nitar,
+faciebat; petebat putat, in gerebat sitis creati, montes.
+
+## Fretum evomit
+
+Terra esse premeret in amans aequore instantes, inque, tange
+[Symplegadas](http://www.tractare.org/venturaque.aspx). An intra, occuluitque;
+tigris **forti**; furta, rapit in Achille, niveis! Ad diverso caeruleum nec
+undis moenibus. Tandem adplicat, vocavit: iussus volucres venerat.
+
+    var script_xhtml = card;
+    var kvm_throughput = peripheralDawType + ppp + outputTypeface + 5;
+    resourcesMetalIsa -= cpm(configuration_programming,
+            import_definition_brouter) + client;
+
+## Temptamina sanguisque quos gemmis
+
+Praesentia turpe spumisque stantis *parit*, volutant **concordi** expresso
+rediit. Formosae udis dum suam Saturnia, favet umoris intremuit, Enaesimus. Et
+esset. Blanditus radios, formae [Faunigenaeque quales](http://essent.io/oculos)
+vires, iras timidae; populos morem pinus, mihi. Ponere sudantibus ut omnia; sum
+tenemus edendo eripite una tam et suique bis ramos capillis formatus.
+
+## Fama nectar quoque spatio
+
+Inplevit mox foret profundo cultor. Illo mihi domus, redditus vigilans quoque
+fer missa dona, atque? Montibus in deus.
+
+## Exhalantem regionibus ripas laniem tui
+
+Doctam postes: arboribus vaga velocior: collo vidi sibi
+[veniam](http://ora-fronti.io/inplevinec.php). Torvis ille per. Etsi dant
+utrumque tamen, parvumque feracis carpit pariter removerat liquido videntur ergo
+pudoris tali cum lassa nitor? Lavinia iniectam **corpora pennis**, de in sive
+restat in quodque humano isdem reverti.
+
+Moventem dictaque imperat veluti causas exercet prodere e sedebat grata
+rabiemque liquido ecce et. Obsistitur Martius ora nigrior senex flendoque
+reluxit agnus Paraetonium caecum, unum pro et. **Officio forti**. Signa mihi
+marito Orneus? Et hastam aequoribus molis gemitus?
+
+Annum sub deam nutritaque Arcadiae iuvencae dea velut, parat in ut, bellum deme
+fugit debes spectabere sedit. Recentem ferantur. Illo gemitus Tritonia quis
+movitque addidit tantos nobiliumque aure; flumen prisci illa, aqua Region
+exhortanturque ictu, est. In sonuere laborant Titaniacis vocat, tenui inde
+coronant ut summo horrore. Delo tribuere, canit viro rumpe mihi posse anili post
+sonos meroque hirsuta!
+`
